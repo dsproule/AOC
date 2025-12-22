390c73531a80692593804668e5211615eb72d03e
Donovan Sproule <donovan.sproule@yahoo.com>
2025-12-22 09:58:25 -0800

    fully optimized

--- aoc2.py
+++ aoc2.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 f = open("input2.txt", "r")
 
 def get_digs(n: int) -> list[int]:
@@ -7,6 +9,7 @@ def get_digs(n: int) -> list[int]:
         n //= 10
     return i
 
+@lru_cache
 def count_combs(n: int) -> int:
     n_digs = get_digs(n)
 
@@ -14,10 +17,11 @@ def count_combs(n: int) -> int:
     for j in range(2, n_digs + 1, 2):
         cur_base = 10 ** (j // 2)
 
-        l = 10 ** (j // 2 - 1)
+        l = 10 ** (j // 2 - 1) - 1
         k = min(cur_base - 1, n // (cur_base + 1))
-        for i in range(l, k + 1):
-            cum_sum += i * (cur_base + 1)
+
+        tmp_sum = (k * k + k - l * l - l) // 2
+        cum_sum += (cur_base + 1) * tmp_sum
     return cum_sum
 
 id_sum = 0
@@ -27,4 +31,5 @@ for id_range in f.read().split(','):
     id_sum += count_combs(end) - count_combs(start - 1)
     
 print(f"Id sum is: {id_sum}")
+print(f"Correct: {38437576669 == id_sum}")
 f.close()
