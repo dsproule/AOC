e6714f031b883b6a5d9c8b87a6a00107477a770d
Donovan Sproule <donovan.sproule@yahoo.com>
2025-12-22 09:27:59 -0800

    aoc2 works with math theory

--- aoc2.py
+++ aoc2.py
@@ -1,20 +1,33 @@
 f = open("input2.txt", "r")
-def digs_n(n: int) -> int:
+
+def get_digs(n: int) -> list[int]:
     i = 0
     while n > 0:
         i += 1
         n //= 10
     return i
 
+def count_combs(n: int) -> int:
+    n_digs = get_digs(n)
+
+    cum_sum = 0
+    cnt = 0
+    for j in range(2, n_digs + 1, 2):
+        l = 10 ** (j // 2 - 1)
+        k = 10 ** (j // 2) - 1
+        for i in range(l, k + 1):
+            tmp_sum = i * (10 ** (j // 2) + 1)
+            if tmp_sum <= n:
+                cnt += 1
+                cum_sum += tmp_sum
+    return cum_sum
+
 id_sum = 0
+p = 0
 for id_range in f.read().split(','):
     start, end = map(int, id_range.split('-'))
+
+    id_sum += count_combs(end) - count_combs(start - 1)
     
-    for i in range(start, end + 1):
-        if (digs := digs_n(i)) % 2 != 0:
-            continue
-        split_digs = 10**(digs // 2)
-        if i // split_digs == i % split_digs:
-            id_sum += i
 print(f"Id sum is: {id_sum}")
 f.close()
