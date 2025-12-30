0ac86b0f37291b3499ec57f485d4969dc760a411
Donovan Sproule <donovan.sproule@yahoo.com>
2025-12-30 10:32:38 -0800

    checkpoint

--- sw/aoc5-2.py
+++ sw/aoc5-2.py
@@ -3,10 +3,13 @@ f = open("input5.txt", "r")
 merged_intvs, intvs = [], []
 
 cum_sum = 0
+i = 0
 for line in f:
     if line == "\n":
         intvs.sort()
         for start, end in intvs:
+            print(start, end)
+            i += 1
             if merged_intvs and start <= merged_intvs[-1][1]:
                 prev_start, prev_end = merged_intvs.pop()
                 start = prev_start
@@ -18,6 +21,6 @@ for line in f:
 
     start, end = map(int, line[:-1].split("-"))
     intvs.append([start, end])
-
+print(i)
 print(f"Result: {cum_sum}")
 f.close()
--- sw/aoc5_hw.cpp
+++ sw/aoc5_hw.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <span>
 #include <cassert>
+#include <algorithm>
 
 const size_t MAX_WIDTH = 256;
 
@@ -10,73 +11,94 @@ using tuple_pair_t = std::pair<uint64_t, uint64_t>;
 
 class Sorter {
  private:
+    static inline void cmp_swp(tuple_pair_t& n1, tuple_pair_t& n2, bool asc) {
+        if ((n1.first > n2.first) == asc) 
+            std::swap(n1, n2);
+    } 
+
     template<size_t N>
-    void bitonic_sort(std::span<tuple_pair_t, N> pairs) {
+    constexpr void merger_N(std::span<tuple_pair_t, N> pairs) {
+        if (N == 1) return;
+
         // sorting network
         const unsigned int reach = N / 2;
 
-        for (int i = 0; i < reach; i++) {
-            if (pairs[i].first > pairs[i + reach].first) 
-                std::swap(pairs[i], pairs[i + reach]);
-        }
+        for (int i = 0; i < reach; i++)
+            cmp_swp(pairs[i], pairs[i + reach], true);
+        
+        merger_N(pairs.template subspan<0, reach>());
+        merger_N(pairs.template subspan<reach, reach>());
     }
 
-    void sort_4(std::span<tuple_pair_t, 4> pairs, bool asc) {
-        if ((pairs[0].first > pairs[2].first) == asc) std::swap(pairs[0], pairs[2]);
-        if ((pairs[1].first > pairs[3].first) == asc) std::swap(pairs[1], pairs[3]);
-        
-        if ((pairs[0].first > pairs[1].first) == asc) std::swap(pairs[0], pairs[1]);
-        if ((pairs[2].first > pairs[3].first) == asc) std::swap(pairs[2], pairs[3]);
+    void sort_8(std::span<tuple_pair_t, 8> pairs, bool asc) {
+        // topologies 
+        // https://bertdobbelaere.github.io/sorting_networks.html#N8L19D6
 
-        if ((pairs[1].first > pairs[2].first) == asc) std::swap(pairs[1], pairs[2]);
+        // stage 1
+        for (int i = 0; i < 6; i = i + 4) {
+            cmp_swp(pairs[i], pairs[i + 2], asc);
+            cmp_swp(pairs[i + 1], pairs[i + 3], asc);
+        }
+        
+        // stage 2
+        for (int i = 0; i < 4; i++)
+            cmp_swp(pairs[i], pairs[i + 4], asc);
+        
+        // stage 3
+        for (int i = 0; i < 8; i = i + 2)
+            cmp_swp(pairs[i], pairs[i + 1], asc);
+        
+        // stage 4
+        for (int i = 2; i < 4; i++)
+            cmp_swp(pairs[i], pairs[i + 2], asc);
+        
+        // stage 5
+        for (int i = 1; i < 4; i = i + 2)
+            cmp_swp(pairs[i], pairs[i + 3], asc);
+        
+        // stage 6
+        for (int i = 1; i < 7; i = i + 2)
+            cmp_swp(pairs[i], pairs[i + 1], asc);
     }
 
  public:
-    void merger_sort_8(std::span<tuple_pair_t, 8> pairs) {
+    void bitonic_sort_16(std::span<tuple_pair_t, 16> pairs) {
         // sort first and second half indep
-        sort_4(pairs.subspan<0, 4>(), 1);
-        sort_4(pairs.subspan<4, 4>(), 0);
+        sort_8(pairs.subspan<0, 8>(), true);
+        sort_8(pairs.subspan<8, 8>(), false);
 
-        // stage1        
-        bitonic_sort(pairs.subspan<0, 8>());
+        // half cleaner
+        const unsigned int reach = 16 / 2;
+        for (int i = 0; i < reach; i++)
+            cmp_swp(pairs[i], pairs[i + reach], true);
 
-        // stage2        
-        bitonic_sort(pairs.subspan<0, 4>());
-        bitonic_sort(pairs.subspan<4, 4>());
-
-        // stage3        
-        bitonic_sort(pairs.subspan<0, 2>());
-        bitonic_sort(pairs.subspan<2, 2>());
-        bitonic_sort(pairs.subspan<4, 2>());
-        bitonic_sort(pairs.subspan<6, 2>());
-
+        // recursive_merge
+        merger_N(pairs.subspan<0, 8>());
+        merger_N(pairs.subspan<8, 8>());
     }
 };
 
 class Mem {
  public:
     void store_mem(uint64_t start, uint64_t end, int i) {
-        bram_start_bank_[i] = start;
-        bram_end_bank_[i] = end;
+        assert (i < bram_start_bank_.size());
+        bram_start_bank_[i][0] = { start, end };
     }
 
-    std::pair<uint64_t, uint64_t> load_mem(int i) {
-        return { bram_start_bank_[i], bram_end_bank_[i] };
+    tuple_pair_t load_mem(int i) {
+        return { bram_start_bank_[i][0] };
     }
 
  private: 
-    uint64_t bram_start_bank_[MAX_WIDTH];
-    uint64_t bram_end_bank_[MAX_WIDTH];
+    std::array<std::array<tuple_pair_t, 2>, 256> bram_start_bank_;
 };
 
 int main() {
     std::ifstream infile("input5.txt");
     std::string line;
 
-    Mem mem_inst = Mem();
-    Sorter sort_inst = Sorter();
-    int i = 0;
     uint64_t start, end;    
+    std::vector<tuple_pair_t> stream;
     while (std::getline(infile, line)) {
         size_t dash_pos = line.find('-');
         if (dash_pos != std::string::npos) {
@@ -87,16 +109,70 @@ int main() {
             start = std::stoull(start_str);
             end = std::stoull(end_str);
             
-            mem_inst.store_mem(start, end, i++);
+            stream.push_back({ start, end });
         }
     }
-    std::vector<tuple_pair_t> pairs;
-    for (int i = 0; i < 8; i++)
-        pairs.push_back(mem_inst.load_mem(i));
-    sort_inst.merger_sort_8(std::span<tuple_pair_t, 8>(pairs.data(), 8));
-    std::cout << pairs.size() << "\n";
-    for (const auto& pair : pairs)
-        std::cout << "(" << pair.first << ", " << pair.second << ")\n";
+    
+    constexpr int LEFT_REGS_END = 16;
+    constexpr int RIGHT_REGS_END = 32;
+    constexpr int MAX_VALUES = 16;
+
+    // Main algorithm
+    Mem mem_inst = Mem();
+    Sorter sort_inst = Sorter();
+
+    // load stream into dual banks
+    for (int i = 0; i < stream.size(); i++)
+        mem_inst.store_mem(stream[i].first, stream[i].second, i);
+
+    std::vector<std::vector<tuple_pair_t>> pairs(2);
+    // loop
+    int mem_i = 0;
+    while (mem_i < stream.size()) {
+        pairs[0].clear();
+        pairs[1].clear();
+
+        std::cout << "mem_i: " << mem_i << "\n";
+        for (int j = mem_i; j < mem_i + MAX_VALUES; j++)
+            pairs[0].push_back(mem_inst.load_mem(j));
+        for (int j = mem_i + MAX_VALUES; j < mem_i + 2 * MAX_VALUES; j++)
+            pairs[1].push_back(mem_inst.load_mem(j));
+
+        // do two sorts
+        std::span<tuple_pair_t, 16> regs_0(pairs[0].data(), 16);
+        std::span<tuple_pair_t, 16> regs_1(pairs[1].data(), 16);
+        
+        sort_inst.bitonic_sort_16(regs_0);
+        sort_inst.bitonic_sort_16(regs_1);
+
+        // load into [0:32]
+        int left_regs_ptr = 0, right_regs_ptr = 0;
+        while (left_regs_ptr < MAX_VALUES && right_regs_ptr < MAX_VALUES) {
+            if (pairs[0][left_regs_ptr] < pairs[1][right_regs_ptr]) {
+                mem_inst.store_mem(pairs[0][left_regs_ptr].first, pairs[0][left_regs_ptr].second, mem_i);
+                left_regs_ptr++;
+            } else {
+                mem_inst.store_mem(pairs[1][right_regs_ptr].first, pairs[1][right_regs_ptr].second, mem_i);
+                right_regs_ptr++;
+            }
+            mem_i++;
+        }
+
+        while (left_regs_ptr < MAX_VALUES) {
+            mem_inst.store_mem(pairs[0][left_regs_ptr].first, pairs[0][left_regs_ptr].second, mem_i++);
+            left_regs_ptr++;
+        }
+        while (right_regs_ptr < MAX_VALUES) {
+            mem_inst.store_mem(pairs[1][right_regs_ptr].first, pairs[1][right_regs_ptr].second, mem_i++);
+            right_regs_ptr++;
+        }
+
+        for (int j = mem_i - 32; j < mem_i; j++) {
+            tuple_pair_t pair = mem_inst.load_mem(j);
+            std::cout << "(" << pair.first << ", " << pair.second << ")\n";
+        }
+    }
+    std::cout << mem_i << "\n";
     
     infile.close();
     return 0;
