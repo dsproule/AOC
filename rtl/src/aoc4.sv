1f3b0fa4a7fdffa122c4cde60b21e581bdfeca59
Donovan Sproule <donovan.sproule@yahoo.com>
2026-01-10 16:54:36 -0800

    decided the aoc4 previous solution was near-optimal for parameters so reverted

--- rtl/src/aoc4.sv
+++ rtl/src/aoc4.sv
@@ -11,132 +11,98 @@ module top(
     output int   updates_out
 );
 
+    logic ack, busy, re_run;
+    logic read_en, write_en;
+    logic [`BANK_ADDR_WIDTH-1:0] row_addr_in;
+    logic [`TX_DATA_WIDTH-1:0]   partial_vec_in, bank_partial_vec_out;
+    logic [`COL_ADDR_WIDTH-1:0]  col_addr_in;
+
     // Declare machine output signals as arrays
-    logic [`MACH_N-1:0] ack, sync_ack, sync_last_row_req, mach_done;
-    logic re_run, sync_init, sync_end;
+    int updates [`MACH_N-1:0];
+    logic [`MACH_N-1:0] mach_changed_out, mach_done_out, mach_write_en, mach_read_en;
+    logic [`BANK_ADDR_WIDTH-1:0] mach_row_addr_out [`MACH_N-1:0];
+    logic [`COL_ADDR_WIDTH-1:0] mach_col_addr_out [`MACH_N-1:0];
+    logic [`TX_DATA_WIDTH-1:0] mach_partial_vec_out [`MACH_N-1:0];
 
-    assign sync_init = (sync != '0);
+    mem main_mem (
+        .clock(clock), .reset(reset),
+        .write_en(write_en), .read_en(read_en), .pad_en(pad_en),
+        .row_addr_in(row_addr_in),
+        .partial_vec_in(partial_vec_in),
+        .col_addr_in(col_addr_in),
+        
+        .ack(ack), .busy(busy),
+        .partial_vec_out(bank_partial_vec_out)
+    );
 
-    logic [`TX_DATA_WIDTH-1:0]   bank_partial_vec_out [`MACH_N-1:0];
-    logic [`BANK_ADDR_WIDTH-1:0] mach_row_addr        [`MACH_N-1:0];
-    logic [`COL_ADDR_WIDTH-1:0]  mach_col_addr        [`MACH_N-1:0];
+    assign mem_ack_out = ack;
+    assign mem_busy_out = busy;
 
-    assign mem_ack_out = |ack;
+    logic [`MACH_N-1:0] reqs, gnt;
+    int arb_i;
 
-    // Debug signals
-    logic tb_packet_write_en_dbg;
-    logic [$clog2(`MAX_ROWS)-1:0] tb_packet_row_addr_dbg;
+    arb arbiter(
+        .clock(clock), .reset(reset),
+        .reqs_in(reqs), .ack_in(ack),
 
-    assign tb_packet_row_addr_dbg = tb_packet_in.row_addr;
-    assign tb_packet_write_en_dbg = tb_packet_in.write_en;
+        .gnt_out(gnt), .i_out(arb_i)
+    );
 
     genvar mach_i;
     generate 
         for (mach_i = 0; mach_i < `MACH_N; mach_i++) begin : mach_gen
-            logic read_en, write_en;
-            logic mach_ack_in, mach_write_en, local_read_en;
+            int start_row_dbg, end_row_dbg;
 
-            logic [`BANK_ADDR_WIDTH-1:0] row_addr_in;
-            logic [`TX_DATA_WIDTH-1:0]   partial_vec_in, mach_partial_vec_in, mach_partial_vec_out, local_bank_partial_vec_out;
-            logic [`COL_ADDR_WIDTH-1:0] col_addr_in;
-                
-            localparam start_row = (mach_i == '0)          ?     '0        : mach_i * `BANK_DEPTH;
-            localparam end_row   = (mach_i == `MACH_N - 1) ? `MAX_ROWS - 1 : mach_i * `BANK_DEPTH + `BANK_DEPTH - 1;
+            assign start_row_dbg = mach_i * `MACH_ROWS;
+            assign end_row_dbg   = (mach_i == `MACH_N - 1) ? `MAX_ROWS : mach_i * `MACH_ROWS + `MACH_ROWS + 1;
 
-            int end_row_dbg, start_row_dbg;
-            assign start_row_dbg = start_row;
-            assign end_row_dbg = end_row;
-
-            assign mach_partial_vec_in = (sync_init) ? bank_partial_vec_out[mach_i - 1] : 
-                                         (sync_end)  ? bank_partial_vec_out[mach_i + 1] : local_bank_partial_vec_out;                                        
-            assign mach_ack_in         = (sync_init) ? ack[mach_i - 1] : 
-                                         (sync_end)  ? ack[mach_i + 1] : ack[mach_i];
+            assign reqs[mach_i] = (mach_write_en[mach_i] | mach_read_en[mach_i]);
 
             freemachine #(
-                .start_row(start_row), .end_row(end_row)
+                .start_row(mach_i * `MACH_ROWS), .end_row((mach_i == `MACH_N - 1) ? `MAX_ROWS : mach_i * `MACH_ROWS + `MACH_ROWS + 1)
             ) mach (
                 .clock(clock), .reset(reset),
-                .partial_vec_in(mach_partial_vec_in),
-                .ack_in(!tb_packet_in.staging && mach_ack_in), .sync_init_in(sync_init),
+                .partial_vec_in(bank_partial_vec_out),
+                .run(run_in | re_run), .ack_in(gnt[mach_i] && ack && !tb_packet_in.staging),
 
-                .changed_out(), .done_out(mach_done[mach_i]),
-                .write_en_out(mach_write_en), .read_en_out(local_read_en), 
-                .sync_ack_out(sync_ack[mach_i]), .sync_last_row_req_out(sync_last_row_req[mach_i]),
-                .row_addr_out(mach_row_addr[mach_i]), .col_addr_out(mach_col_addr[mach_i]),
-                .partial_vec_out(mach_partial_vec_out), .updates_out()
+                .changed_out(mach_changed_out[mach_i]), .done_out(mach_done_out[mach_i]),  
+                .write_en_out(mach_write_en[mach_i]), .read_en_out(mach_read_en[mach_i]),
+                .row_addr_out(mach_row_addr_out[mach_i]), .col_addr_out(mach_col_addr_out[mach_i]),
+                .partial_vec_out(mach_partial_vec_out[mach_i]), .updates_out(updates[mach_i])
             );
-
-            logic tb_packet_in_range;
-            assign tb_packet_in_range = tb_packet_in.staging && (start_row <= tb_packet_in.row_addr && tb_packet_in.row_addr <= end_row);
-
-            // MUX between testbench control and machine control
-            always_comb begin
-                if (tb_packet_in.staging) begin
-                    partial_vec_in = tb_packet_in.partial_vec;
-                    row_addr_in    = tb_packet_in.row_addr;
-                    col_addr_in    = tb_packet_in.col_addr;
-                    read_en        = tb_packet_in.read_en;
-                    write_en       = (tb_packet_in_range) ? tb_packet_in.write_en    : 1'b0;
-                end else begin
-                    partial_vec_in = mach_partial_vec_out;
-                    row_addr_in    = (sync_init) ? `BANK_ADDR_WIDTH'(-1) : 
-                                     (sync_end)  ? `BANK_ADDR_WIDTH'(0)  : mach_row_addr[mach_i];
-                    col_addr_in    = (sync_init) ? mach_col_addr[mach_i + 1] : 
-                                     (sync_end)  ? mach_col_addr[mach_i - 1] : mach_col_addr[mach_i];
-                    read_en        = sync_init || local_read_en || sync_end;
-                    write_en       = mach_write_en;
-                end
-            end
-
-            // create bank that fits within space
-            mem bank (
-                .clock(clock), .reset(reset),
-                .write_en(write_en), .read_en(read_en), .pad_en(pad_en),
-                .row_addr_in(row_addr_in),
-                .partial_vec_in(partial_vec_in),
-                .col_addr_in(col_addr_in),
-                
-                .ack_out(ack[mach_i]),
-                .partial_vec_out(local_bank_partial_vec_out)
-            );
-            
-            assign bank_partial_vec_out[mach_i] = local_bank_partial_vec_out;
         end 
     endgenerate
 
-    logic [1:0] sync;
+    int add_i;
+    logic run_started, final_sum;
     always_ff @(posedge clock) begin
-        if      (reset)  sync <= '0;
-        else if (run_in) sync <= 2'b01;
-
-        if (&sync_ack && sync_init) sync <= 2'b00;
+        if (reset) begin
+            re_run      <= 1'b0;
+            run_started <= 1'b0;
+            updates_out <=   '0;
+            add_i       <=   '0;
+            final_sum   <= 1'b0;
+        end else if (final_sum && add_i < `MACH_N) begin
+            add_i       <= add_i + 1;
+            updates_out <= updates_out + updates[add_i];
+        end else if (run_started) begin
+            re_run <= 1'b0;
+            if (&mach_done_out) begin
+                if (~|mach_changed_out) final_sum <= 1'b1;
+                else re_run <= 1'b1;
+            end
+        end else if (run_in) run_started <= 1'b1;
     end
 
-    assign sync_end = &sync_last_row_req[`MACH_N-2:0];
+    assign done_out = (add_i == `MACH_N);
 
-    // int add_i;
-    // logic run_started, final_sum;
-    // always_ff @(posedge clock) begin
-    //     if (reset) begin
-    //         re_run      <= 1'b0;
-    // //         run_started <= 1'b0;
-    // //         updates_out <=   '0;
-    // //         add_i       <=   '0;
-    // //         final_sum   <= 1'b0;
-    //     end else if (final_sum && add_i < `MACH_N) begin
-    // //         add_i       <= add_i + 1;
-    // //         updates_out <= updates_out + updates[add_i];
-    //     end else if (run_started) begin
-    // //         re_run <= 1'b0;
-    // //         if (&mach_done_out) begin
-    // //             if (~|mach_changed_out) final_sum <= 1'b1;
-    // //             else re_run <= 1'b1;
-    //         // end
-    //     end else if (run_in) run_started <= 1'b1;
-    // end
-
-    // assign done_out = (add_i == `MACH_N);
-    assign done_out = &mach_done;
-
+    // MUX between testbench control and machine control
+    assign partial_vec_in = (tb_packet_in.staging) ? tb_packet_in.partial_vec : mach_partial_vec_out[arb_i];
+    assign row_addr_in    = (tb_packet_in.staging) ? tb_packet_in.row_addr    : mach_row_addr_out[arb_i];
+    assign col_addr_in    = (tb_packet_in.staging) ? tb_packet_in.col_addr    : mach_col_addr_out[arb_i];
+    assign read_en        = (tb_packet_in.staging) ? tb_packet_in.read_en     : 
+                            (|gnt)                 ? mach_read_en[arb_i]      : 1'b0;
+    assign write_en       = (tb_packet_in.staging) ? tb_packet_in.write_en    : 
+                            (|gnt)                 ? mach_write_en[arb_i]     : 1'b0;
 
 endmodule
--- rtl/src/aoc4_freemachine.sv
+++ rtl/src/aoc4_freemachine.sv
@@ -4,11 +4,10 @@ module freemachine #(
 )(
     input logic clock, reset,
     input logic [`TX_DATA_WIDTH] partial_vec_in,
-    input logic run, ack_in, sync_init_in,
+    input logic run, ack_in,
 
     output int updates_out,
-    output logic changed_out, done_out, write_en_out, read_en_out, 
-    output logic sync_last_row_req_out, sync_ack_out,
+    output logic changed_out, done_out, write_en_out, read_en_out,
     output logic [`BANK_ADDR_WIDTH-1:0] row_addr_out,
     output logic [`COL_ADDR_WIDTH-1:0]  col_addr_out,
     output logic [`TX_DATA_WIDTH]       partial_vec_out
@@ -17,8 +16,7 @@ module freemachine #(
     localparam log2_mod = $clog2(`TX_DATA_WIDTH);
 
     logic regs_valid, done_out_buf;
-    // tail reg is the first value
-    logic [`GRID_VEC_ALIGN_N-1:0] regs [3], tail_reg;
+    logic [`GRID_VEC_ALIGN_N-1:0] regs [3];
     logic [`BANK_ADDR_WIDTH-1:0] row_addr_out_buf;
 
     assign done_out = done_out_buf && !write_en_out;
@@ -30,11 +28,11 @@ module freemachine #(
     assign regs_dbg_1 = regs[1];
     assign regs_dbg_2 = regs[2];
 
-    logic prune, next_last_row;
+    logic prune, last_row;
     logic [3:0] degree;
     logic [`GRID_VEC_ALIGN_N-1:0] next_regs_1, next_regs_2, next_regs_0;
 
-    assign next_last_row = (row_addr_out_buf == end_row - start_row - 1);
+    assign last_row = (row_addr_out_buf == end_row);
 
     // accumulation 
     always_comb begin
@@ -53,7 +51,7 @@ module freemachine #(
         if (col_i == `GRID_VEC_ALIGN_N - 1) begin
             next_regs_0 = next_regs_1;
             next_regs_1 = next_regs_2;
-            next_regs_2 = '0;
+            next_regs_2 = -1;
         end
     end
 
@@ -64,25 +62,11 @@ module freemachine #(
     assign read_en_out = (write_en_out || done_out) ? 1'b0 : read_en_buf;
 
     // reg loading handler. (timing of counters)
-    logic sync_valid, prev_sync; 
-    always_ff @(posedge clock) begin
-        if (reset) prev_sync <= 1'b0;
-        else       prev_sync <= sync_init_in;
-    end
-
-    assign sync_ack_out = sync_init_in && (insert_reg == 1);
-    assign sync_valid   = sync_init_in && (prev_sync != sync_init_in);
-
-    logic stall;
-    assign stall = write_en_out || sync_ack_out;
-
     always_ff @(posedge clock) begin
         if (reset) begin
             {regs[0], regs[1], regs[2]} <= '0;
             regs_valid <= 1'b0;
-            insert_reg <= '0;
-        end else if (sync_valid) begin
-            // this is sync top
+        end else if (run) begin
             regs_valid <= 1'b0;
 
             insert_reg <= '0;
@@ -90,12 +74,12 @@ module freemachine #(
                 regs[0]      <= '0;
                 insert_reg   <= 1;
             end
-        end else if (stall) begin
+        end else if (write_en_out) begin
         end else if (ack_in && !regs_valid) begin
             regs[insert_reg][`VEC_OFFSET(col_addr_out) +: `TX_DATA_WIDTH] <= partial_vec_in;
 
-            if (col_addr_out + `TX_DATA_WIDTH >= `GRID_VEC_ALIGN_N) begin
-                if (insert_reg != 2) begin
+            if (col_addr_out + `TX_DATA_WIDTH >= `GRID_VEC_ALIGN_N || last_row) begin
+                if (insert_reg != 2 && !last_row) begin
                     insert_reg   <= insert_reg + 1;
                 end else begin
                     regs_valid  <= 1'b1;
@@ -104,21 +88,20 @@ module freemachine #(
             end 
         end else if (regs_valid && !done_out_buf && !write_en_out) begin
             {regs[0], regs[1], regs[2]} <= {next_regs_0, next_regs_1, next_regs_2};
-            if (col_i == `GRID_VEC_ALIGN_N - 1 && end_row + 1 != `MAX_ROWS) regs_valid <= 1'b0;
+            if (col_i == `GRID_VEC_ALIGN_N - 1) regs_valid <= 1'b0;
         end
     end
 
     // memory interactions machine
     always_ff @(posedge clock) begin
         if (reset) begin
-            done_out_buf      <= 1'b0;
-            updates_out       <= '0;
-            store_parity      <= '0;
-            changed_out       <= 1'b1;
-            sync_last_row_req_out <= 1'b0;
+            done_out_buf     <= 1'b0;
+            updates_out      <= '0;
+            store_parity <= '0;
+            changed_out  <= 1'b1;
             
             read_en_buf  <= 1'b0;
-        end else if (sync_valid) begin
+        end else if (run) begin
             // place initial values and set the machine to go. Initializer 
             // of machine in a block.
             col_addr_out <=  '0;
@@ -127,47 +110,44 @@ module freemachine #(
             done_out_buf <=  '0;
             changed_out  <= 1'b0;
 
-            row_addr_out_buf <= '0;
-            row_i <= start_row;
-        end else if (stall) begin
-            if (write_en_out && ack_in) begin
+            if (start_row == 0) begin
+                row_addr_out_buf <= start_row;
+            end else row_addr_out_buf <= start_row - 1;
+
+        end else if (write_en_out) begin
+            if (ack_in) begin
                 store_parity[1] <= ~store_parity[1];
                 col_addr_out <= col_i;
             end
+            // used to block execution of system
         end else if (ack_in && !regs_valid) begin
             // save chunks until end of line. latency-insensitive design,
             // helps contention of mem if each can be stalled
-            if (col_addr_out + `TX_DATA_WIDTH < `GRID_VEC_ALIGN_N) begin
+            if (col_addr_out + `TX_DATA_WIDTH < `GRID_VEC_ALIGN_N && !last_row) begin
                 col_addr_out <= col_addr_out + `TX_DATA_WIDTH;
-            end else if (insert_reg != 2) begin
-                if (!sync_init_in) row_addr_out_buf <= row_addr_out_buf + 1;
+            end else if (insert_reg != 2 && !last_row) begin
+                row_addr_out_buf <= row_addr_out_buf + 1;
                 col_addr_out <= '0;
             end else begin
-                sync_last_row_req_out <= 1'b0;
                 col_i       <= '0;
                 read_en_buf <= 1'b0;
             end
 
         end else if (regs_valid && !done_out_buf && !write_en_out) begin
             // driver of the cycle
-            // if (prune) begin
-            //     updates_out <= updates_out + 1;
-            //     changed_out <= 1'b1;
-            // end
+            if (prune) begin
+                updates_out <= updates_out + 1;
+                changed_out <= 1'b1;
+            end
             
             if (col_i == `GRID_VEC_ALIGN_N - 1) begin
-                col_addr_out <= '0;
+                if (last_row)
+                    done_out_buf <= 1'b1;
+                read_en_buf <= 1'b1;
                 col_i <= '0;
-                row_i <= row_i + 1;
+                col_addr_out <= '0;
 
-                if (row_i == end_row) done_out_buf <= 1'b1;
-                else if (row_i == end_row - 1) begin
-                    sync_last_row_req_out <= (end_row + 1 != `MAX_ROWS);
-                end else begin
-                    read_en_buf <= 1'b1;
-                    row_addr_out_buf <= row_addr_out_buf + 1;
-                end
-
+                row_addr_out_buf <= row_addr_out_buf + 1;
             end else col_i <= col_i + 1;
             
             if (col_i[log2_mod-1:0] == log2_mod'(-1)) begin
@@ -178,11 +158,10 @@ module freemachine #(
         end
     end
     
-    assign write_en_out = (store_parity[0] != store_parity[1]) && (start_row == 4);
+    assign write_en_out = (store_parity[0] != store_parity[1]);
     assign partial_vec_out = (col_i == '0) ? regs[0][`GRID_VEC_ALIGN_N - 1 -: `TX_DATA_WIDTH] : 
                                              regs[1][`GRID_VEC_ALIGN_N - 1 -: `TX_DATA_WIDTH] ;
     assign row_addr_out = (!write_en_out)  ? row_addr_out_buf     : 
                           (col_i == '0)    ? row_addr_out_buf - 2 : row_addr_out_buf - 1;
-    // assign row_addr_out = row_addr_out_buf;
     
 endmodule
